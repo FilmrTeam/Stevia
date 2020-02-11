24cb2147fa483ca7990673a08a4b33e643cb2b0c
Bernardo Alecrim <bernardo@filmrapp.com>
2020-02-11 19:58:16 -0300

    Created new Xcode project with shared scheme for Stevia

---
+++ Stevia/Stevia.h
@@ -0,0 +1,19 @@
+//
+//  Stevia.h
+//  Stevia
+//
+//  Created by Bernardo Alecrim on 11/02/20.
+//  Copyright © 2020 Filmr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Stevia.
+FOUNDATION_EXPORT double SteviaVersionNumber;
+
+//! Project version string for Stevia.
+FOUNDATION_EXPORT const unsigned char SteviaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stevia/PublicHeader.h>
+
+
